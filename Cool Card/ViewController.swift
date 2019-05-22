f5adfe8adaf7e6e2816f7b69a33fb037b0feb6aa
Kou <koubarlow@gmail.com>
2019-05-22 15:25:20 -0600

    Initial Commit

---
+++ Cool Card/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cool Card
+//
+//  Created by Kou Barlow on 5/22/19.
+//  Copyright © 2019 Kou Barlow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
